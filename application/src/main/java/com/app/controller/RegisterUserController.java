ceb8ab189e29f203654b6aa46e8799b4cf86b21b
kalyan.karbhar111@gmail.com <kalyan.karbhar111@gmail.com>
2022-03-30 23:45:29 +0530

    logging basic set up

--- application/src/main/java/com/app/controller/RegisterUserController.java
+++ application/src/main/java/com/app/controller/RegisterUserController.java
@@ -5,6 +5,8 @@ import java.util.List;
 
 import javax.validation.Valid;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -16,6 +18,8 @@ import org.springframework.web.bind.annotation.RestController;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
+import lombok.extern.slf4j.Slf4j;
+
 import com.app.response.ErrorResponse;
 import com.app.services.RegisterUserService;
 import com.aap.response.RegisterUserResponse;
@@ -23,9 +27,11 @@ import com.aap.response.UserDetail;
 import com.app.constants.ApplicationConstant;
 import com.app.request.RegisterUserRequest;
 
+@Slf4j
 @RestController
 public class RegisterUserController {
-
+	Logger log = LoggerFactory.getLogger(RegisterUserController.class);
+	
 	private static final String USER_REGISER = "This URL is used to register the user";
 	
 	@Autowired
@@ -40,9 +46,10 @@ public class RegisterUserController {
 	})
 	@PostMapping(ApplicationConstant.VERSION+ApplicationConstant.USERS)
 	public ResponseEntity<RegisterUserResponse> registerUser(@RequestBody @Valid RegisterUserRequest request){
-		
+		log.info("Registering the user for the request received = {}",request);
 		RegisterUserResponse response = new RegisterUserResponse();
 		response.setUserDetail(registerUserService.saveUserDetail(request));
+		log.info("stored the user details and retruing response = {}",response);
 		return new ResponseEntity<>(response ,HttpStatus.OK);
 		
 	}

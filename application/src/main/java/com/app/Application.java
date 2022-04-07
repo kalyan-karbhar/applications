3b7496a7bb9314539def247070d09342f894c49a
kalyan.karbhar111@gmail.com <kalyan.karbhar111@gmail.com>
2022-04-08 01:45:54 +0530

    Application property file change the location

--- application/src/main/java/com/app/Application.java
+++ application/src/main/java/com/app/Application.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 import springfox.documentation.builders.RequestHandlerSelectors;
@@ -14,8 +15,9 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @ComponentScan(basePackages={"com.app"})
 @EnableWebMvc
 @EnableSwagger2
-@SpringBootApplication
-public class Application {
+@SpringBootApplication  
+@PropertySource(value ="${spring.config.location}${spring.config.name}.properties",ignoreResourceNotFound =true)
+ public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
